a959df2cc7a8c39fa5002c4bd9d0729d60f80c33
David Lasry <davidlasry2000@gmail.com>
2024-02-13 17:47:47 +0200

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-// import { useState } from "react";
-
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
+import GamePage from "./pages/GamePage";
 
 function App() {
-  return <></>;
+  return (
+    <div className="p-3 container text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
+      hello
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/play" element={<GamePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
